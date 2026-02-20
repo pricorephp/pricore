f611d68e6bc76a9e03607e2122dac47f64309b18
Maarten Bode <maarten.bode@zonneplan.nl>
2026-02-20 17:05:30 +0100

    feat: support wildcard for trusted proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -27,7 +27,13 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
-        $middleware->trustProxies(at: explode(',', env('TRUSTED_PROXIES', '')));
+        $trustedProxies = explode(',', env('TRUSTED_PROXIES', ''));
+
+        if ($trustedProxies === ['*']) {
+            $trustedProxies = '*';
+        }
+
+        $middleware->trustProxies(at: $trustedProxies);
 
         $middleware->web(append: [
             HandleAppearance::class,
